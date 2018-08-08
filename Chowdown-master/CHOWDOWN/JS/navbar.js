a498c092a37b8d72333c7596cc8259ca18f5daa5
Chiara BoisAubin <chiara.boisaubin@gmail.com>
2018-08-07 21:25:01 -0700

    adding the the codes.

---
+++ Chowdown-master/CHOWDOWN/JS/navbar.js
@@ -0,0 +1,9 @@
+// Nav bar //
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
---
+++ Chowdown-master/ChowDown -User-Restaurant/src/One.java
@@ -0,0 +1,16 @@
+
+public class One {
+
+	public static void main(String[] args) {
+		int open = 0;
+		int close = 0;
+		
+		for (open = 700; open < 230; open+=30) {
+			if(){
+				
+			}
+		}
+
+	}
+
+}
